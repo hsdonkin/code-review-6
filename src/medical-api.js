6df97f86c732d9fda457801f36ef613ed3d607f6
Hugh Donkin <hsdonkin@gmail.com>
2019-07-05 10:11:35 -0700

    query api based on symptoms or doctor name first, middle, last

--- src/medical-api.js
+++ src/medical-api.js
@@ -1,4 +1,5 @@
-const apiKey = process.env.API_KEY;
+
+let apiKey = "2e559e877e70796d71ab32693a9e5c87";
 
 export function symptomSearch(symptomName){
 
@@ -27,5 +28,28 @@ export function symptomSearch(symptomName){
 }
 
 export function doctorSearch(doctorName){
+
+  let doctorPromise = new Promise(function(resolve,reject){
+    let request = new XMLHttpRequest();
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${apiKey}&location=or-portland&name=${doctorName}`;
+
+    request.onload = function(){
+      if (this.status === 200) {
+        resolve(request.response);
+        console.log("Api responded!");
+      }else {
+        reject(Error(request.statusText));
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+  });
+
+  doctorPromise.then(function(response){
+    let apiResult = JSON.parse(response);
+    console.log(apiResult);
+  })
 
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const Dotenv = require('dotenv').config({path: path.join(__dirname, '.env')});
+const Dotenv = require('dotenv-webpack');
 
       module.exports = {
         entry: './src/main.js',
